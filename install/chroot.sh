6cbcd330c061104d6030dd28c2640e9e3d782991
daed4lus <vmichel@intechinfo.fr>
2018-10-29 18:41:08 +0100

    further correction

--- install/chroot.sh
+++ install/chroot.sh
@@ -3,6 +3,6 @@ ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
 hwclock --systohc
 sed -i 's/#en_US*/en_US/' /etc/locale.gen && locale-gen && printf 'LANG=en_US.UTF-8' > /etc/locale.conf 
 echo 'Skylab' > /etc/hostname && printf '127.0.0.1 Skylab\n::1 Skylab\n10.0.2.15 air.esiea.fr' > /etc/hosts
-pacman -S grub intel-ucode
+pacman -S --noconfirm grub intel-ucode
 grub-install --target=i386-pc /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
