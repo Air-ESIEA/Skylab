f68b9fb481fcccf1bbc4c43fe232bd1073930c18
AlphonseDanltas <p.mercier.h@gmail.com>
2018-11-12 11:57:24 +0100

    correction

--- services/samba/samba_install.sh
+++ services/samba/samba_install.sh
@@ -2,9 +2,9 @@ pacman -S --noconfirm samba
 wget https://raw.githubusercontent.com/Air-ESIEA/Skylab/master/services/samba/smb.conf.default -O /etc/samba/smb.conf.default
 cp /etc/samba/smb.conf{.default,}
 mkdir -p /shares/Air-ESIEA_share01
+groupadd -r sambausers
 chown -R root:sambausers /shares/Air-ESIEA_share01
 chmod 1770 /shares/Air-ESIEA_share01
-groupadd -r sambausers
 
 # #Add userX to the sambausers group
 # sudo passwd sambausers -a userX
