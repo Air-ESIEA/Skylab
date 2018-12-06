c572ee037b616fe0afa05f2b4e3ddb3c095823fa
daed4lus <vmichel@intechinfo.fr>
2018-12-06 06:19:19 +0100

    added gitea

---
+++ services/gitea/gitea.sh
@@ -0,0 +1,1 @@
+cp services/gitea/ /etc/gitea/ -r
--- services/services.sh
+++ services/services.sh
@@ -1,8 +1,17 @@
+source bind/bind.sh
+source dovecot/dovecot.sh
+source postfix/postfix.sh
+source httpd/httpd.sh
+source iptables/iptables.sh
+source samba/samba.sh
+source gitea/gitea.sh
+
+
 systemctl enable named.service
 systemctl enable samba.service
 systemctl enable httpd.service
 systemctl enable iptables.service
 systemctl enable postfix.service
 systemctl enable dovecot.service
-#systemctl enable 
+systemctl enable gitea.service
 #systemctl enable 
