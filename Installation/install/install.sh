543feac7ed666890c72809851063c9f7170ee570
Squoimote <guillaume.torquet@gmail.com>
2018-12-09 19:40:34 +0100

    fix view

--- Installation/install/install.sh
+++ Installation/install/install.sh
@@ -15,28 +15,89 @@ echo -e "${NC}"
 
 #Program
 echo -e "\t\t${YELLOW}[Running]${WHITE} Set datetime${NC}"
-timedatectl set-ntp 1
-echo -e "\x1b\x5b\x41\t\t${GREEN}[Done]${WHITE} Set datetime${NC}"
+timedatectl set-ntp 1 &> $LOG 2> $ERROR
+if [$? -ne 0]
+then {
+  echo -e "\x1b\x5b\x41\t\t${YELLOW}[Done]${WHITE} Set datetime${NC}   "
+  echo -e "${BG_ORANGE}${RED}[Error]${WHITE} One or more errors appeared, refer to error.log"
+}
+else {
+  echo -e "\x1b\x5b\x41\t\t${GREEN}[Done]${WHITE} Set datetime${NC}   "
+}
+echo $ERROR >> $STACK_ERROR
+
+
 
 echo -e "\t\t${YELLOW}[Running]${WHITE} Partitioning${NC}"
-sfdisk /dev/sda < part
-echo -e "\x1b\x5b\x41\t\t${GREEN}[Done]${WHITE} Partitioning${NC}"
+sfdisk /dev/sda < part &> $LOG 2> $ERROR
+if [$? -ne 0]
+then {
+  echo -e "\x1b\x5b\x41\t\t${YELLOW}[Done]${WHITE} Partitioning${NC}   "
+  echo -e "${BG_ORANGE}${RED}[Error]${WHITE} One or more errors appeared, refer to error.log"
+}
+else {
+  echo -e "\x1b\x5b\x41\t\t${GREEN}[Done]${WHITE} Partitioning${NC}   "
+}
+echo $ERROR >> $STACK_ERROR
+
 
 echo -e "\t\t${YELLOW}[Running]${WHITE} Formatting and mounting${NC}"
-mkfs.ext4 /dev/sda2 && mkfs.ext4 /dev/sda3 && mount /dev/sda2 /mnt && mkdir /mnt/storage && mount /dev/sda3 /mnt/storage
-echo -e "\x1b\x5b\x41\t\t${GREEN}[Done]${WHITE} Formatting and mounting${NC}"
+mkfs.ext4 /dev/sda2 && mkfs.ext4 /dev/sda3 && mount /dev/sda2 /mnt && mkdir /mnt/storage && mount /dev/sda3 /mnt/storage &> $LOG 2> $ERROR
+if [$? -ne 0]
+then {
+  echo -e "\x1b\x5b\x41\t\t${YELLOW}[Done]${WHITE} Formatting and mounting${NC}   "
+  echo -e "${BG_ORANGE}${RED}[Error]${WHITE} One or more errors appeared, refer to error.log"
+}
+else {
+  echo -e "\x1b\x5b\x41\t\t${GREEN}[Done]${WHITE} Formatting and mounting${NC}   "
+}
+echo $ERROR >> $STACK_ERROR
+
 
 echo -e "\t\t${YELLOW}[Running]${WHITE} Installation of basic packages${NC}"
-pacstrap /mnt base base-devel
-echo -e "\x1b\x5b\x41\t\t${GREEN}[Done]${WHITE} Installation of basic packages${NC}"
+pacstrap /mnt base base-devel &> $LOG 2> $ERROR
+if [$? -ne 0]
+then {
+  echo -e "\x1b\x5b\x41\t\t${YELLOW}[Done]${WHITE} Installation of basic packages${NC}   "
+  echo -e "${BG_ORANGE}${RED}[Error]${WHITE} One or more errors appeared, refer to error.log"
+}
+else {
+  echo -e "\x1b\x5b\x41\t\t${GREEN}[Done]${WHITE} Installation of basic packages${NC}   "
+}
+echo $ERROR >> $STACK_ERROR
+
 
 echo -e "\t\t${YELLOW}[Running]${WHITE} Generate fstab file${NC}"
-ls /mnt
-genfstab -U /mnt >> /mnt/etc/fstab
-echo -e "\x1b\x5b\x41\t\t${GREEN}[Done]${WHITE} Generate fstab file${NC}"
+genfstab -U /mnt >> /mnt/etc/fstab &> $LOG 2> $ERROR
+if [$? -ne 0]
+then {
+  echo -e "\x1b\x5b\x41\t\t${YELLOW}[Done]${WHITE} Generate fstab file${NC}   "
+  echo -e "${BG_ORANGE}${RED}[Error]${WHITE} One or more errors appeared, refer to error.log"
+}
+else {
+  echo -e "\x1b\x5b\x41\t\t${GREEN}[Done]${WHITE} Generate fstab file${NC}   "
+}
+echo $ERROR >> $STACK_ERROR
+
 
 echo -e "\t\t${YELLOW}[Run]${WHITE} Chroot script${NC}"
-cp chroot.sh /mnt/root/
-cp list-pkg /mnt/root/
-arch-chroot /mnt /root/chroot.sh
-echo -e "\t\t${GREEN}[Done]${WHITE} Chroot script${NC}"
+{
+  cp chroot.sh /mnt/root/
+  cp list-pkg /mnt/root/
+  if [-e "log.log"]
+  then
+    cp log.log /mnt/root/
+  if [-e "error.log"]
+  then
+    cp error.log /mnt/root
+  arch-chroot /mnt /root/chroot.sh
+} &> $LOG 2> $ERROR
+if [$? -ne 0]
+then {
+  echo -e "\t\t${YELLOW}[Done]${WHITE} Chroot script${NC}"
+  echo -e "${BG_ORANGE}${RED}[Error]${WHITE} One or more errors appeared, refer to error.log"
+}
+else {
+  echo -e "\t\t${GREEN}[Done]${WHITE} Chroot script${NC}"
+}
+echo $ERROR >> $STACK_ERROR
--- Installation/start_install.sh
+++ Installation/start_install.sh
@@ -19,14 +19,44 @@ echo -e "${NC}"
 #Program
 clear
 echo -e "${BG_ORANGE}${YELLOW}[Downloading]${WHITE} Installation scripts${NC}"
-pacman -Sy --noconfirm git
-git clone http://github.com/Air-ESIEA/Skylab
-echo -e "\x1b\x5b\x41${BG_ORANGE}${GREEN}[Downloaded]${WHITE} Installation scripts${NC}"
+{
+  pacman -Sy --noconfirm git
+  git clone http://github.com/Air-ESIEA/Skylab
+} &> $LOG 2> $ERROR
+if [$? -ne 0]
+then {
+  echo -e "\x1b\x5b\x41${BG_ORANGE}${YELLOW}[Downloaded]${WHITE} Installation scripts${NC} "
+  echo -e "${BG_ORANGE}${RED}[Error]${WHITE} One or more errors appeared, refer to error.log"
+}
+else {
+  echo -e "\x1b\x5b\x41${BG_ORANGE}${GREEN}[Downloaded]${WHITE} Installation scripts${NC} "
+}
+echo $ERROR >> $STACK_ERROR
+
+
+echo $LOG >> log.log
+echo $STACK_ERROR >> error.log
+
 
 echo -e "\t${BG_WHITE}${YELLOW}[Run]${WHITE} Installation script${NC}"
 cd Skylab/Installation/install
 ./install.sh
-echo -e "\t${BG_WHITE}${GREEN}[Done]${WHITE} Installation script${NC}"
+if [$? -ne 0]
+then {
+  echo -e "\t${BG_WHITE}${YELLOW}[Done]${WHITE} Installation script${NC}"
+  echo -e "${BG_ORANGE}${RED}[Error]${WHITE} One or more errors appeared, refer to error.log"
+}
+else {
+  echo -e "\t${BG_WHITE}${GREEN}[Done]${WHITE} Installation script${NC}"
+}
+echo $ERROR >> $STACK_ERROR
+
 
 echo -e "${BG_WHITE}${RED}[Finished]${WHITE} Please check error.log to be sure the installation is correct${NC}"
 echo "Tape 'reboot' and press enter to reboot"
+if [-e "/mnt/root/log.log"]
+then
+  cp /mnt/root/log.log ~/
+if [-e "/mnt/root/error.log"]
+then
+  cp /mnt/root/error.log ~/
